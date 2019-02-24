a221b2eef439d3e49e236f7c26484006aeb82701
Nataniel Carvalho <natanielmendes@hotmail.com>
2019-02-24 13:49:16 -0300

    port as env var to run in heroku

--- config/express.js
+++ config/express.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const message = require('../api/validation/messageManager')
 const app = express();
@@ -8,8 +9,9 @@ app.use((err, req, res, next) => {
     if (err instanceof SyntaxError)
         return res.status(400).json({error: message.ErrorMessage('INVALID_SYNTAX')})
 })
-app.listen('3000', function() {
-    console.log('App listening on port 3000!')
+
+app.listen(process.env.PORT, function() {
+    console.log('App listening on port ' + process.env.PORT + '!')
 })
 
 module.exports = app
